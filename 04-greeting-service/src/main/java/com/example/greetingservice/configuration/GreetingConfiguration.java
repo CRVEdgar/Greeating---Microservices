a44401a5b78da051e8a8ff400905eb726485f0ce
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-11-04 03:22:19 -0300

    alteracao das configuracao para atualizacao automatica do servidor de configuracao sem interromper a aplicacao

--- 04-greeting-service/src/main/java/com/example/greetingservice/configuration/GreetingConfiguration.java
+++ 04-greeting-service/src/main/java/com/example/greetingservice/configuration/GreetingConfiguration.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("greeting-service") //anotacao que determinar a configuracao do servico
+@RefreshScope //atualiza as automaticamente apos a alteacao do servidor de configuracao
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
