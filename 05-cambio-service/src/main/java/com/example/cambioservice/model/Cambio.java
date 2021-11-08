e8fd0892c99a4ddc09d3182053026585ec17b261
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-11-07 23:55:42 -0300

    criacao do controlle, repository e configuraçao da migration com massa de dados

--- 05-cambio-service/src/main/java/com/example/cambioservice/model/Cambio.java
+++ 05-cambio-service/src/main/java/com/example/cambioservice/model/Cambio.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
@@ -12,14 +13,28 @@ import java.math.BigDecimal;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "cambio")
 public class Cambio implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
+    @Column(name = "from_currency", nullable = false, length = 3)
     private String from;
+
+    @Column(name = "to_currency", nullable = false, length = 3)
     private String to;
+
+    @Column(nullable = false)
     private BigDecimal conversionFactor;
+
+    @Transient
     private BigDecimal convertedValue;
+
+    @Transient
     private String enviroment;
 }
---
+++ 05-cambio-service/src/main/resources/db/migration/V1__Create_Table_Cambio.sql
@@ -0,0 +1,10 @@
+﻿CREATE SEQUENCE camb_id_seq;
+CREATE TABLE cambio (
+  id int not null default nextval('camb_id_seq'),
+  from_currency CHAR(3) NOT NULL,
+  to_currency CHAR(3) NOT NULL,
+  conversion_factor decimal(65,2) NOT NULL,
+
+  primary key (id)
+);
+ALTER SEQUENCE camb_id_seq OWNED BY cambio.id;
