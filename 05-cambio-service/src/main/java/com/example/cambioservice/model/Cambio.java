e5668fef18e732df6593e811a72982a1ae3caed9
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-11-07 21:14:14 -0300

    criando entidade Cambio e endpoint para acesso

---
+++ 05-cambio-service/src/main/java/com/example/cambioservice/model/Cambio.java
@@ -0,0 +1,25 @@
+package com.example.cambioservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cambio implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionFactor;
+    private BigDecimal convertedValue;
+    private String enviroment;
+}
