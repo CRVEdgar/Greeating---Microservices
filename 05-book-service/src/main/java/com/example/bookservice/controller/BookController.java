9280c85e8dbfda2e6dd7320183143c60fe5761d3
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-11-11 21:14:44 -0300

    consumindo endpoint cambioService pelo service BookService

--- 05-book-service/src/main/java/com/example/bookservice/controller/BookController.java
+++ 05-book-service/src/main/java/com/example/bookservice/controller/BookController.java
@@ -2,14 +2,17 @@ package com.example.bookservice.controller;
 
 import com.example.bookservice.model.Book;
 import com.example.bookservice.repository.BookRepository;
+import com.example.bookservice.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "book-service")
@@ -25,9 +28,25 @@ public class BookController {
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency){
         var book = repository.findById(id).orElseThrow( () -> new RuntimeException("Livro nao encontrado"));
 
+        HashMap<String, String> params = new HashMap<>();
+
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8000/cambio-service/" +
+                                "{amount}/{from}/{to}",
+                        Cambio.class, //classe que sera convertida a informação vinda do endpoint
+                        params);
+
+        var cambio = response.getBody();
+
         var port =  environment.getProperty("local.server.port");
 
         book.setEnvironment(port);
+        book.setPrice(cambio.getConvertedValue());
         return book;
     }
 }
