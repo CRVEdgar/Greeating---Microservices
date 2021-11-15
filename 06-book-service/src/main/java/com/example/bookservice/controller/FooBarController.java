de1733262bd10b3a321da43450401666aef3706f
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-11-14 21:38:08 -0300

    configurando o tempo de duracao da chamada de endpoint com falha utilizando Resillence4J

--- 06-book-service/src/main/java/com/example/bookservice/controller/FooBarController.java
+++ 06-book-service/src/main/java/com/example/bookservice/controller/FooBarController.java
@@ -3,8 +3,10 @@ package com.example.bookservice.controller;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,11 +17,18 @@ public class FooBarController {
     private Logger logger =  LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @Retry(name = "foo-bar") // name c/c instances:foo-bar: [.yml]
+    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") // name c/c instances:foo-bar: [.yml] // fallbackMethod = "fallbackMethod" c/c String fallbackMethod(Exception e)
     public String foobar(){
         logger.info("MESSAGE: Request for foo-bar is received!");
         var response = new RestTemplate()
                 .getForEntity("http://localhost:8080/foo-bar", String.class);
         return response.getBody();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    private String fallbackMethod(Exception e){
+        //metodo sera invocado apos a tentativa de N chamadas ao endpoint /foo-bar
+        // captura a exception e executa this metodo
+        return "MESSAGE: fallbackMethod foo-bar";
     }
 }
